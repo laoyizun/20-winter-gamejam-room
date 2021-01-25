c9baf62cbbf15ee5be483b3ff0ea9eb8e3afed70
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 14:26:23 +0800

    更新 pxt.json, main.ts, hyeroom.ts, liziroom.ts, wjyroom.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
+gamejam.registerRoom(function(){
+   // gamejam.drawNextRoom('栗子', 'Flow Control Room')
+  //  liziroom.init()
+})
+gamejam.registerRoom(function(){
+    gamejam.drawNextRoom('黄宇恩','Adventure Room')
+    hyeroom.init()
+})
+gamejam.registerRoom(function(){
+ //   gamejam.drawNextRoom('吴俊毅','Maze Room')
+  //  wjyroom.init()
+})
+tiles.setTilemap(tilemap`级别1`)
 
+gamejam.startGameJam() 
