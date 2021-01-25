4fe523d686313a0efc76a261d6c608b1174db91e
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 23:11:10 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 gamejam.registerRoom(function(){
-    gamejam.drawNextRoom('黄宇恩','Adventure Room')
+    gamejam.drawNextRoom('黄宇恩','xx Room')
     hyeroom.init()
+})
+gamejam.registerRoom(function(){
+    gamejam.drawNextRoom('吴俊毅','xx Room')
+    wjyroom.init()
 })
 gamejam.startGameJam()
