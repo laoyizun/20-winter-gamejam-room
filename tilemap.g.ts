fc70e8e10267364f6708406df06ce4919192a8d9
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 16:15:48 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -8,11 +8,57 @@ namespace myTiles {
     export const hyetile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const hyetile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile7 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "级别1":
-            case "级别1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020202010202020202020202020202020202020202020202020201020202020202020202020201020202020202020202020202020202020202020202020202020202020202020202020202020102020202020102020201020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202010202020202020202020201020202020202020202020202020202020202020202020202020202020102020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
+            case "hyelevel":
+            case "hyelevel":return tiles.createTilemap(hex`100010000a020202020202020202020202020202020202010202020202020202020202020202020202020202020201020202020202020202020201020202020202020202020202020202020202020202020202020202020202020202020202020102020202020102020201020202020202020202020202020202020202020202020202020802020202020202020202020202020208020202020202020202010202020202080202020201020202020202020202020802020202020202020202020202020208020102020202020202020202020202080202020202020202020202020202020802020202020202020202020202020207060606090302040205020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile1,myTiles.tile5,myTiles.tile6,myTiles.tile7,sprites.castle.tileDarkGrass1], TileScale.Sixteen);
+            case "hyelevel_0":
+            case "hyelevel_0":return tiles.createTilemap(hex`10001000070404040404040404040404040404050311111111111111111111111111110603111111111111111111111111111106031111111111111111111111111111060311111111111111111111111111110603111111111111111111111111111106031111111111110f0e10111111111106031111111111110c000a111111111106031111111111110d090b11111111110603111111111111111111111111111106031111111111111111111111111111060311111111111111111111111111110603111111111111111111111111111106031111111111111111111111111111060311111111111111111111111111110601020202020202020202020202020208`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.purpleOuterSouth2,sprites.dungeon.purpleOuterEast2,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterWest2,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterNorthEast,sprites.castle.tilePath5], TileScale.Sixteen);
+            case "hyelevel_1":
+            case "hyelevel_1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202020202010202020202020202020202020202020202020202020201020202020202020202020201020202020202020202020202020202020202020202020202020202020202020202020202020102020202020102020201020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202010202020202020202020201020202020202020202020202020202020202020202020202020202020102020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202020202`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -37,9 +83,13 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "hyetile2":return hyetile2;
-            case "hyetile3":return hyetile3;
-            case "hyetile4":return hyetile4;
+            case "hyetile2":return tile2;
+            case "hyetile3":return tile3;
+            case "hyetile4":return tile4;
+            case "hyetile1":return tile1;
+            case "hyetile5":return tile5;
+            case "hyetile6":return tile6;
+            case "hyetile7":return tile7;
         }
         return null;
     })
