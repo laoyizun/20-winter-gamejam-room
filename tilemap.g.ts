c3343a014db829486ee4c23c41763f83d4ab3daa
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 14:35:11 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,7 +2,12 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
-
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const hyetile4 = image.ofBuffer(hex``);
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "级别1":
