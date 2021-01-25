019fb597954b7034a0599c00e351ec5f4fda1ecd
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 18:02:20 +0800

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -17,67 +17,67 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const hyetile7 = image.ofBuffer(hex``);
      //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile5 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile6 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile7 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile8 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile9 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile10 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile11 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile12 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile13 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile14 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile15 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile16 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile17 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile18 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile19 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile20 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile21 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile22 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile23 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile24 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile25 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile26 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile27 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile28 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile29 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile30 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile31 = image.ofBuffer(hex``);
+//     export const tile1 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile2 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile3 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile4 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile5 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile6 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile7 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile8 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile9 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile10 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile11 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile12 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile13 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile14 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile15 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile16 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile17 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile18 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile19 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile20 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile21 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile22 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile23 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile24 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile25 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile26 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile27 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile28 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile29 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile30 = image.ofBuffer(hex``);
+//     //% fixedInstance jres blockIdentity=images._tile
+//     export const tile31 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -207,37 +207,37 @@ namespace myTiles {
             case "hyetile5":return hyetile5;
             case "hyetile6":return hyetile6;
             case "hyetile7":return hyetile7;
-            case "tile1":return tile1;
-            case "tile2":return tile2;
-            case "tile3":return tile3;
-            case "tile4":return tile4;
-            case "tile5":return tile5;
-            case "tile6":return tile6;
-            case "tile7":return tile7;
-            case "tile8":return tile8;
-            case "tile9":return tile9;
-            case "tile10":return tile10;
-            case "tile11":return tile11;
-            case "tile12":return tile12;
-            case "tile13":return tile13;
-            case "tile14":return tile14;
-            case "tile15":return tile15;
-            case "tile16":return tile16;
-            case "tile17":return tile17;
-            case "tile18":return tile18;
-            case "tile19":return tile19;
-            case "tile20":return tile20;
-            case "tile21":return tile21;
-            case "tile22":return tile22;
-            case "tile23":return tile23;
-            case "tile24":return tile24;
-            case "tile25":return tile25;
-            case "tile26":return tile26;
-            case "tile27":return tile27;
-            case "tile28":return tile28;
-            case "tile29":return tile29;
-            case "tile30":return tile30;
-            case "tile31":return tile31;
+//             case "tile1":return tile1;
+//             case "tile2":return tile2;
+//             case "tile3":return tile3;
+//             case "tile4":return tile4;
+//             case "tile5":return tile5;
+//             case "tile6":return tile6;
+//             case "tile7":return tile7;
+//             case "tile8":return tile8;
+//             case "tile9":return tile9;
+//             case "tile10":return tile10;
+//             case "tile11":return tile11;
+//             case "tile12":return tile12;
+//             case "tile13":return tile13;
+//             case "tile14":return tile14;
+//             case "tile15":return tile15;
+//             case "tile16":return tile16;
+//             case "tile17":return tile17;
+//             case "tile18":return tile18;
+//             case "tile19":return tile19;
+//             case "tile20":return tile20;
+//             case "tile21":return tile21;
+//             case "tile22":return tile22;
+//             case "tile23":return tile23;
+//             case "tile24":return tile24;
+//             case "tile25":return tile25;
+//             case "tile26":return tile26;
+//             case "tile27":return tile27;
+//             case "tile28":return tile28;
+//             case "tile29":return tile29;
+//             case "tile30":return tile30;
+//             case "tile31":return tile31;
         }
         return null;
     })
