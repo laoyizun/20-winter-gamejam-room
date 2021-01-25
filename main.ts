6310cb21acb23c874c63be21090ce597b5701430
Lawrence <44716177+laoyizun@users.noreply.github.com>
2021-01-25 15:31:37 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-gamejam.registerRoom(function(){
-   // gamejam.drawNextRoom('栗子', 'Flow Control Room')
-  //  liziroom.init()
-})
+
 gamejam.registerRoom(function(){
     gamejam.drawNextRoom('黄宇恩','Adventure Room')
     hyeroom.init()
 })
-gamejam.registerRoom(function(){
- //   gamejam.drawNextRoom('吴俊毅','Maze Room')
-  //  wjyroom.init()
-})
-gamejam.startGameJam() 
+gamejam.startGameJam()
